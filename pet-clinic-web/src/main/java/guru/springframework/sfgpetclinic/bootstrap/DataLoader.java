3cfd6980e1f889ee645bd55fc09e2acfa114b968
pkrysztofiak <p.krysztofiak@protonmail.com>
2021-10-24 13:24:30 +0200

    Bootstrap data Closes #14

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,54 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
+import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner michaelWeston = new Owner();
+        michaelWeston.setId(1l);
+        michaelWeston.setFirstName("Michael");
+        michaelWeston.setLastName("Weston");
+        ownerService.save(michaelWeston);
+
+        Owner fionaGlenanne = new Owner();
+        fionaGlenanne.setId(2l);
+        fionaGlenanne.setFirstName("Fiona");
+        fionaGlenanne.setLastName("Glenanne");
+        ownerService.save(fionaGlenanne);
+
+        System.out.println("Owners initialized");
+
+        Vet samAxe = new Vet();
+        samAxe.setId(1L);
+        samAxe.setFirstName("Sam");
+        samAxe.setLastName("Axe");
+        vetService.save(samAxe);
+
+        Vet jessiePorter = new Vet();
+        jessiePorter.setId(2L);
+        jessiePorter.setFirstName("Jessie");
+        jessiePorter.setLastName("Porter");
+
+        vetService.save(jessiePorter);
+
+        System.out.println("Vets initialized");
+    }
+}

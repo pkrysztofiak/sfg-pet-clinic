40f503755f91596d1eab91260163501dd4220b81
pkrysztofiak <p.krysztofiak@protonmail.com>
2021-11-14 12:33:15 +0100

    Add jpa tests for owner service Closes #50

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSpringDataJPAServiceTest.java
@@ -0,0 +1,66 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerSpringDataJPAServiceTest {
+
+    @Mock
+    OwnerRepository ownerRepository;
+
+    @InjectMocks
+    OwnerSpringDataJPAService service;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findByLastName() {
+        when(ownerRepository.findByLastName(any())).thenReturn(Owner.builder().id(1L).lastName("Smith").build());
+        Owner smith = service.findByLastName("Smith");
+        assertEquals("Smith", smith.getLastName());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+
+        when(ownerRepository.findAll()).thenReturn(owners);
+
+        assertEquals(owners, service.findAll());
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
